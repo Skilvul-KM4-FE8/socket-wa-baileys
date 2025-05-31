10130b640aa3ff18520f2b26c5f0d3aba8e6cde4
Muhamad Alfito Santosa <sanskun02pg@gmail.com>
2025-05-31 23:24:27 +0700

    push cors

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 require("dotenv").config();
-const app = require("./app");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+// Konfigurasi CORS untuk mengizinkan semua origin
+app.use(
+  cors({
+    origin: "*", // Mengizinkan semua domain
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Metode yang diizinkan
+    allowedHeaders: ["Content-Type", "Authorization"], // Header yang diizinkan
+  })
+);
+
 const { connectToWhatsApp } = require("./services/whatsappService");
 
 const PORT = process.env.PORT || 3000;
